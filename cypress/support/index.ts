1c1a5feed689fcbbf73c5f52fc51c372473a6a10
Jaril <jarilvalenciano@gmail.com>
2022-11-02 08:15:36 -0700

    Bump cypress version

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+// // @ts-ignore
+// require('@replayio/cypress/support');
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
